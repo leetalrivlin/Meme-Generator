29231b52250efaf80f397fa937c2b86333058e7c
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-02-10 18:20:30 +0200

    get data from gMeme

--- js/main.js
+++ js/main.js
@@ -3,28 +3,47 @@ var gElCanvas;
 var gCtx;
 
 function onInit() {
-    onRenderGallery();
-    gElCanvas = document.getElementById('my-canvas');
-    gCtx = gElCanvas.getContext('2d');
+  onRenderGallery();
+  gElCanvas = document.getElementById('my-canvas');
+  gCtx = gElCanvas.getContext('2d');
 }
 
 function onRenderGallery() {
-    let imgs = gImgs;
-    var strHtml = imgs.map((img) => {
-        return `<img src="${img.url}" class="gallery-item item-${img.id}" onclick="onImgClicked(${img.id})"/>`
-}).join('');
-document.querySelector('.img-gallery').innerHTML = strHtml;
+  let imgs = gImgs;
+  var strHtml = imgs
+    .map((img) => {
+      return `<img src="${img.url}" class="gallery-item item-${img.id}" onclick="onImgClicked(${img.id})"/>`;
+    })
+    .join('');
+  document.querySelector('.img-gallery').innerHTML = strHtml;
 }
 
 function onImgClicked(imgId) {
-    drawImg(imgId);
-    createMeme(imgId);
+  gMeme = createMeme(imgId);
+  let img = gMeme.selectedImgId;
+  let txt = gMeme.lines[0].txt;
+  let size = gMeme.lines[0].size;
+  let align = gMeme.lines[0].align;
+  let color = gMeme.lines[0].color;
+
+  drawImg(img);
+  drawTxt(txt, size, align, color);
 }
 
 function drawImg(imgId) {
-    const img = new Image();
-    img.src = `img/gallery-imgs/${imgId}.jpg`;
-    img.onload = () => {
-        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height); //img,x,y,xend,yend
-    }
+  const img = new Image();
+  img.src = `img/gallery-imgs/${imgId}.jpg`;
+  img.onload = () => {
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height); //img,x,y,xend,yend
+  };
+}
+
+function drawTxt(txt, size, align, color) {
+  gCtx.lineWidth = 1;
+  gCtx.strokeStyle = 'black';
+  gCtx.fillStyle = color;
+  gCtx.font = `${size}px Impact`;
+  gCtx.textAlign = align;
+  gCtx.fillText(txt, 100, 100);
+  gCtx.strokeText(txt, 100, 100);
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,29 +1,30 @@
 'use strict';
 var gKeywords = { happy: 12, 'funny puk': 1 };
 var gImgs = [
-    { id: 1, url: 'img/gallery-imgs/1.jpg', keywords: ['funny','man'] },
-    { id: 2, url: 'img/gallery-imgs/2.jpg', keywords: ['cute','puppy','animal'] },
-    { id: 3, url: 'img/gallery-imgs/3.jpg', keywords: ['cute','puppy','animal','baby'] },
-    { id: 4, url: 'img/gallery-imgs/4.jpg', keywords: ['cute','animal'] },
-    { id: 5, url: 'img/gallery-imgs/5.jpg', keywords: ['funny', 'great','baby'] },
-    { id: 6, url: 'img/gallery-imgs/6.jpg', keywords: ['funny','man'] },
-    { id: 7, url: 'img/gallery-imgs/7.jpg', keywords: ['funny','baby'] },
-    { id: 8, url: 'img/gallery-imgs/8.jpg', keywords: ['funny','man'] },
-    { id: 9, url: 'img/gallery-imgs/9.jpg', keywords: ['funny','baby'] },
+  { id: 1, url: 'img/gallery-imgs/1.jpg', keywords: ['funny', 'man'] },
+  { id: 2, url: 'img/gallery-imgs/2.jpg', keywords: ['cute', 'puppy', 'animal'] },
+  { id: 3, url: 'img/gallery-imgs/3.jpg', keywords: ['cute', 'puppy', 'animal', 'baby'] },
+  { id: 4, url: 'img/gallery-imgs/4.jpg', keywords: ['cute', 'animal'] },
+  { id: 5, url: 'img/gallery-imgs/5.jpg', keywords: ['funny', 'great', 'baby'] },
+  { id: 6, url: 'img/gallery-imgs/6.jpg', keywords: ['funny', 'man'] },
+  { id: 7, url: 'img/gallery-imgs/7.jpg', keywords: ['funny', 'baby'] },
+  { id: 8, url: 'img/gallery-imgs/8.jpg', keywords: ['funny', 'man'] },
+  { id: 9, url: 'img/gallery-imgs/9.jpg', keywords: ['funny', 'baby'] },
 ];
 var gMeme;
 
 function createMeme(imgId) {
-    gMeme = {
-        selectedImgId: imgId,
-        selectedLineIdx: 0,
-        lines: [
-          {
-            txt: 'I never eat Falafel',
-            size: 20,
-            align: 'left',
-            color: 'red',
-          },
-        ],
-      };
+  var gMeme = {
+    selectedImgId: imgId,
+    selectedLineIdx: 0,
+    lines: [
+      {
+        txt: 'I never eat Falafel',
+        size: 20,
+        align: 'left',
+        color: 'red',
+      },
+    ],
+  };
+  return gMeme;
 }
